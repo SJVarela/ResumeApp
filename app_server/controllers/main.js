e286242d8a06b397186ed94b4c11cfd29a597a13
Santiago Varela <s.javiervarela@gmail.com>
2019-03-06 09:40:46 -0300

    Trying new design

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,3 +1,21 @@
 module.exports.index = function(req, res) {
-  res.render("index", { title: "Express" });
+  res.render("index", {
+    title: "Santiago Varela Resume",
+    resume: {
+      projects: [
+        {
+          title: "Onilne Resume",
+          description: "This is a online resume",
+          link: "https://github.com/SJVarela/ResumeApp"
+        }
+      ],
+      education: [
+        {
+          inst: "Universidad abierta interamericana",
+          date: "2015 - 2018",
+          degree: "System Engineering"
+        }
+      ]
+    }
+  });
 };
