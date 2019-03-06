c970e8fef3e50ee9a986df856a983f900eceb201
Santiago Varela <s.javiervarela@gmail.com>
2019-03-06 10:08:32 -0300

    Finish preliminary design

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -2,10 +2,15 @@ module.exports.index = function(req, res) {
   res.render("index", {
     title: "Santiago Varela Resume",
     resume: {
+      name: "Santiago Varela",
+      job: "Web developer",
+      platforms: [".NET", "Node.js"],
+      languages: ["C#", "JavaScript"],
       projects: [
         {
           title: "Onilne Resume",
-          description: "This is a online resume",
+          description:
+            "The current resumé, done with Node.js and Pug as the template engine",
           link: "https://github.com/SJVarela/ResumeApp"
         }
       ],
@@ -14,6 +19,11 @@ module.exports.index = function(req, res) {
           inst: "Universidad abierta interamericana",
           date: "2015 - 2018",
           degree: "System Engineering"
+        },
+        {
+          inst: "Universidad de Buenos Aires",
+          date: "2011 - 2013",
+          degree: "Electronic Engineering"
         }
       ]
     }
