cdb1ed49ad06f84b3ecfbba91ce8b2f55ab642b8
SJVarela <s.javiervarela@gmail.com>
2020-10-07 14:48:40 -0300

    Using CDN for Materialize Update CV info

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,4 @@
-module.exports.index = function(req, res) {
+module.exports.index = function (req, res) {
   res.render("index3", {
     title: "Santiago Varela Resume",
     resume: {
@@ -7,110 +7,131 @@ module.exports.index = function(req, res) {
       about: "",
       frameworks: [
         {
+          name: "ASP.NET",
+          imgUri: "images/asp.net-logo.png",
+        },
+        {
           name: ".NET Core",
-          imgUri: "images/netcore-logo.png"
+          imgUri: "images/netcore-logo.png",
         },
         {
           name: "EF Core",
-          imgUri: "images/efcore-logo.png"
+          imgUri: "images/efcore-logo.png",
         },
         {
           name: "NodeJs",
-          imgUri: "images/node-logo.png"
+          imgUri: "images/node-logo.png",
         },
         {
           name: "React",
-          imgUri: "images/react-logo.png"
+          imgUri: "images/react-logo.png",
         },
         {
           name: "Materialize",
-          imgUri: "images/materializecss-logo.png"
-        }
+          imgUri: "images/materializecss-logo.png",
+        },
       ],
       languages: [
         {
           name: "C#",
-          imgUri: "images/c-logo.png"
+          imgUri: "images/c-logo.png",
         },
         {
           name: "SQL",
-          imgUri: "images/sql-logo.png"
+          imgUri: "images/sql-logo.png",
         },
         {
           name: "Javascipt",
-          imgUri: "images/es6-logo.png"
+          imgUri: "images/es6-logo.png",
         },
         {
           name: "TypeScript",
-          imgUri: "images/typescript-logo.png"
+          imgUri: "images/typescript-logo.png",
         },
         {
           name: "HTML5",
-          imgUri: "images/html5-logo.png"
+          imgUri: "images/html5-logo.png",
         },
         {
           name: "CSS3",
-          imgUri: "images/css3-logo.png"
-        }
+          imgUri: "images/css3-logo.png",
+        },
       ],
       databases: [
         {
           name: "SQL Server",
-          imgUri: "images/sqlserver-logo.png"
+          imgUri: "images/sqlserver-logo.png",
+        },
+        {
+          name: "Oracle",
+          imgUri: "images/oracle-logo.png",
         },
         {
           name: "MongoDB",
-          imgUri: "images/mongo-logo.png"
-        }
+          imgUri: "images/mongo-logo.png",
+        },
       ],
       tools: [
         {
           name: "VS Code",
-          imgUri: "images/vs-code-logo.png"
+          imgUri: "images/vs-code-logo.png",
         },
         {
           name: "Visual Studio",
-          imgUri: "images/visual-studio-logo.png"
-        }
+          imgUri: "images/visual-studio-logo.png",
+        },
+        {
+          name: "Postman",
+          imgUri: "images/postman-logo.png",
+        },
       ],
       projects: [
         {
-          title: "Onilne Resume",
+          title: "Online Resume",
           description:
             "An online resumé to showcase my projects. I used Node.Js for the server and Pug as the template engine with Materialize",
-          link: "https://github.com/SJVarela/ResumeApp"
+          link: "https://github.com/SJVarela/ResumeApp",
         },
         {
           title: "Task manager app",
           description:
             "A React app to manage tasks. Done with stateless functional components",
-          link: "https://github.com/SJVarela/TaskManager"
+          link: "https://github.com/SJVarela/TaskManager",
         },
         {
           title: "StoreApp",
           description:
             "A store application to sell some personal items. The server is made with .Net Core, using Entity Framework as the ORM and following the guidelines of clean architecture\n The client was built with Razor pages",
-          link: "https://github.com/SJVarela/StoreApp"
+          link: "https://github.com/SJVarela/StoreApp",
         },
         {
-          title: "BlazorApp",
+          title: "Reakt",
           description:
-            "A test application to test the new features of Razor Components (Blazor)",
-          link: "https://github.com/SJVarela/BlazorTest"
-        }
+            "A collaboration project to provide a REST API for a React SPA",
+          link: "https://github.com/SJVarela/Reakt",
+        },
       ],
       education: [
         {
           inst: "Universidad Abierta Interamericana",
           date: "2015 - 2018",
-          degree: "System Engineering"
+          degree: "System Engineering",
         },
         {
           inst: "Universidad de Buenos Aires",
           date: "2011 - 2013",
-          degree: "Electronic Engineering"
-        }
-      ]
-    }
+          degree: "Electronic Engineering",
+        },
+      ],
+      experience: [
+        {
+          inst: "Hexacta",
+          date: "2019 - present",
+          role: "Developer",
+          description:
+            "Development from design to implementation for CreditOne Bank, using ASP.NET Framework and PL/SQL. Worked mainly in backend code and REST APIs \nAlso contributed in internal applications using ASP.NET + Angular 4",
+        },
+      ],
+    },
   });
 };
--- public/js/init.js
+++ public/js/init.js
@@ -1,20 +1,2 @@
-$(document).ready(function() {
-  $(".parallax").parallax();
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  var elems = document.querySelectorAll(".sidenav");
-  var instances = M.Sidenav.init(elems, { preventScrolling: false });
-});
-
-$(document).ready(function() {
-  $(".scrollspy").scrollSpy();
-});
-
-$(document).ready(function() {
-  $(".fixed-action-btn").floatingActionButton();
-});
-
-$(document).ready(function() {
-  $(".collapsible").collapsible();
-});
+M.AutoInit();
+M.Sidenav.init(elems, { preventScrolling: false });
